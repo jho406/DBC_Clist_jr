2c932e37675f4abe06914a21214c805677b0b026
Apprentice <apprentice@devbootcamp.com>
2013-04-02 16:58:31 -0700

    mvp

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,17 +1,42 @@
 get '/categories' do
-  # Look in app/views/index.erb
-  erb :index
+  @categories = Category.all
+  erb :category_index
 end
 
-get '/categories/:id'
-    
+get '/categories/:id' do
+  @category = Category.find(params[:id]) 
+  @posts = @category.posts.all 
+  erb :post_index
 end
 
-get '/posts'
+get '/categories/:id/posts/new' do
+  erb :new
 end
 
-get '/posts/:id'
+post '/categories/:id/posts/new' do
+  @post = Post.create(name: params[:name], body: params[:body], category_id: params[:id])
+  erb :user_nav
+
+  # redirect to("/posts/#{post.id}/edit?access_key=#{post.access_key}")
 end
 
-# get '/:access_key' do
-# end
+get '/posts/:id/edit' do
+  @post = Post.find(params[:id])
+  pass unless params[:access_key] == @post.access_key 
+  @post = Post.find(params[:id])
+  erb :edit_delete
+end
+
+post '/posts/:id/edit' do
+  post = Post.find(params[:id])
+  pass unless params[:access_key] == post.access_key
+  post.name = params[:name]
+  post.body = params[:body]
+  post.save
+  redirect to("/posts/#{post.id}/edit?access_key=#{post.access_key}")
+end
+
+get '/posts/:id' do
+  @post = Post.find(params[:id])
+  erb :show
+end
